afafe40a1ff3c15d19499d4e0e37761da8ac8854
Allas Assis <allasassis10@gmail.com>
2022-11-13 17:40:12 -0300

    Entity User and REST

---
+++ src/main/java/com/allasassis/workshopmongo/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.allasassis.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.allasassis.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@RequestMapping(method=RequestMethod.GET)	// também pode usar @GetMapping, é a mesma coisa
+	public ResponseEntity<List<User>> findAll() {	// ResponseEntity encapsula toda uma estrutura necessária pra gente retornar respostas http já com possíveis cabeçalhos e erros
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+}
