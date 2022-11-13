663735a00ca613acd81d7918cf5365efada4e89b
Allas Assis <allasassis10@gmail.com>
2022-11-13 20:02:14 -0300

    User insertion with POST

--- src/main/java/com/allasassis/workshopmongo/services/UserService.java
+++ src/main/java/com/allasassis/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.allasassis.workshopmongo.domain.User;
+import com.allasassis.workshopmongo.dto.UserDTO;
 import com.allasassis.workshopmongo.repository.UserRepository;
 import com.allasassis.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	// Colocaremos o fromDTO no service, porque pode ser que ele precise acessar o banco de dados no futuro, e o repositório está instanciado aqui
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		// esse método converte o UserDTO para um User
 	}
 }
