99570a847cdc2c67bb76b11b067a60dc95d0519b
Allas Assis <allasassis10@gmail.com>
2022-11-13 22:40:47 -0300

    Deleting a user with DELETE

--- src/main/java/com/allasassis/workshopmongo/services/UserService.java
+++ src/main/java/com/allasassis/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id); // aqui estamos usando o findById apenas para aproveitar o tratamento de exceção, caso o id não exista
+		repo.deleteById(id);
+	}
+	
 	// Colocaremos o fromDTO no service, porque pode ser que ele precise acessar o banco de dados no futuro, e o repositório está instanciado aqui
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
