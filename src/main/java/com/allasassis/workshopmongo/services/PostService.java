54ccdcd975a488c4ae6b11ecd43751b1831b7a1a
Allas Assis <allasassis10@gmail.com>
2022-11-14 16:30:37 -0300

    Getting a post by id

---
+++ src/main/java/com/allasassis/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.allasassis.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.allasassis.workshopmongo.domain.Post;
+import com.allasassis.workshopmongo.repository.PostRepository;
+import com.allasassis.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired	// autowired praticamente instancia o objeto sozinho
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
+	}
+	
+}
