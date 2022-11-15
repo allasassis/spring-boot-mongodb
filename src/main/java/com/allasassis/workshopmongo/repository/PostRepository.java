741e5afa10f077600a9ae2b8e7916bef7ebe2ab3
Allas Assis <allasassis10@gmail.com>
2022-11-15 15:07:06 -0300

    Simple query with @Query

--- src/main/java/com/allasassis/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/allasassis/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.allasassis.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.allasassis.workshopmongo.domain.Post;
@@ -10,6 +11,12 @@ import com.allasassis.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{ 
 	// o String é porque ele pede o tipo do ID da sua classe, q no caso da User, é string
+	
+	
+	// title = campo pelo qual a gente vai efetuar a busca, ?0 = expressão regular, o primeiro parâmetro que vier no método, se tivesse 2 e 
+	//vc quisesse o segundo seria ?1, i = ignora maiusculas e minusculas
+	@Query("{ title: { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	// achar pelo titulo qualquer post que contenha o argumento text que você passar, ignorando se é maiuscula ou minuscula
